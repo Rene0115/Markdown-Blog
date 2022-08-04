f2e98b3a4ffe2928faa92d6dbee57e8f76da1d3c
Rene0115 <enere0115@gmail.com>
2022-08-04 14:39:08 +0100

    setup database connection

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -1,3 +1,14 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-cycle */
+/* eslint-disable no-unused-vars */
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { logger } from '../app.js';
 
-mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true });
+dotenv.config();
+
+const database = () => {
+  mongoose.connect(process.env.MONGODB_URI).then((value) => logger.info('database connected')).catch((err) => logger.error(err));
+};
+
+export default database;
--- middlewares/index.middleware.js
+++ middlewares/index.middleware.js
@@ -1,11 +1,18 @@
 /* eslint-disable import/no-cycle */
 /* eslint-disable import/extensions */
+import express from 'express';
+import morgan from 'morgan';
+import database from '../config/mongoose.config.js';
 import router from '../routes/index.routes.js';
 import errorMiddleware from './error.middleware.js';
 
 const middleware = (app) => {
   app.use(router);
   app.use(errorMiddleware);
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(morgan());
+  database();
 };
 
 export default middleware;
